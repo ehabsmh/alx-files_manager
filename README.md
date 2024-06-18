f7cdbb6a6645ba3ba4ccf257bfac15dcaed36e3b
ehabsmh <ehabsmh3@gmail.com>
2024-06-18 19:41:23 +0300

    0. Redis utils

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-files_manager
-This project is a summary of a back-end trimester: authentication, NodeJS, MongoDB, Redis, pagination and background processing.
+alx-files_manager
---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
